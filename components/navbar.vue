47f8104bb8d7b2444298ad11084d62b275ba04d6
Nik <nikparadis@hotmail.com>
2021-02-15 21:23:27 -0500

    mobile padding

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- container for fixed nav bar -->
-  <div class="fixed z-20 w-full top-0" ref="navbar">
+  <div class="py-3 md:py-1 fixed z-20 w-full top-0" ref="navbar">
     <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center shadow-sm">
       <div class="flex-1 flex justify-between items-center">
         <nuxt-link to="/">
