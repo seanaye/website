80f39ee7dd0abce68da2bcaf16a6f57fe30e7b3d
Nik <nikparadis@hotmail.com>
2021-02-17 19:41:29 -0500

    fix mobile updating svg bug

--- pages/index.vue
+++ pages/index.vue
@@ -60,16 +60,14 @@
       </a>
 
    <!-- This is an example component -->
-
-
-        <button @click="switchTheme" class="block mt-32 md:mt-40 mb-12 h-10 w-10 mx-auto focus:outline-none text-gray-900">
-          <svg id="moon" class="p-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-          </svg>
-          <svg id="sun" class="hidden p-4 text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
-          </svg>
-        </button>
+      <button @click="switchTheme" class="block mt-32 md:mt-40 mb-12 h-10 w-10 mx-auto focus:outline-none text-gray-900">
+        <svg id="moon" class="p-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+        </svg>
+        <svg id="sun" class="hidden p-4 text-yellow-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
+        </svg>
+      </button>
 
   </div>
 </template>
@@ -97,25 +95,26 @@ export default {
 
   methods: {
     switchTheme() {
-        let htmlClasses = document.querySelector('html').classList;
+      let htmlClasses = document.querySelector('html').classList;
 
-        if(localStorage.theme == 'dark'){
-          htmlClasses.remove('dark');
-          localStorage.removeItem('theme');
-        } else {
-          htmlClasses.add('dark');
-          localStorage.theme = 'dark';
-        }
-
-        var sun = document.getElementById("sun");
-        var moon = document.getElementById("moon");
-        
-        sun.classList.toggle('hidden');
-        moon.classList.toggle('hidden');
-
+      if(localStorage.theme == 'dark'){
+        htmlClasses.remove('dark');
+        localStorage.removeItem('theme');
+      } else {
+        htmlClasses.add('dark');
+        localStorage.theme = 'dark';
       }
-    },
 
+      var sun = document.getElementById("sun");
+      var moon = document.getElementById("moon");
+      
+      sun.classList.toggle('hidden');
+      moon.classList.toggle('hidden');
+
+    }
+
+  },
+  //ensure proper svg is shown
     beforeUpdate(){
       var sun = document.getElementById("sun");
       var moon = document.getElementById("moon");
@@ -123,9 +122,11 @@ export default {
         let htmlClasses = document.querySelector('html').classList;
         htmlClasses.add('dark');
         console.log("am run dark");
-        moon.classList.toggle('hidden');
-        sun.classList.toggle('hidden');
-    }
+        if (sun.classList.contains('hidden')){
+          moon.classList.toggle('hidden');
+          sun.classList.toggle('hidden');
+        };
+      }
     }
 };
 </script>
